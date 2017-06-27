eb8ac93942f10c84426d6f4d13e2c48829d2ba21
CA23 <CA23@users.noreply.github.com>
2017-06-27 16:57:18 +0200

    separated class definitions and main

--- Calc.py
+++ Calc.py
@@ -82,22 +82,3 @@ class Interpret:
 
     
 
-
-def main():
-    while True:
-        try:
-            text = raw_input('Calculate > ')
-        except EOFError:
-            #EOFError is raised when raw_input() ends without reading any data
-            #For instance a Ctrl + D ends current read which could rasie an EOF
-            break
-        if not text:
-            #if no text is entered
-            continue
-        expression = Interpret(text)
-        result = expression.evaluate()
-        print result
-
-
-if __name__ == "__main__":
-    main()
